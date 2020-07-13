3295e9a7d668ba678d6f8bba863485bf42287e9f
Sobaniec <konrad.sobaniec@capgemini.com>
2020-07-13 14:31:43 +0200

    sign up component, working sign up

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import FormInput from '../form-input/form-input'
 import CustomButton from '../custom-button/custom-button'
 
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+
 import "./sign-up.scss";
 
 class SignUp extends Component {
@@ -10,36 +12,78 @@ class SignUp extends Component {
     super(props)
 
     this.state = {
+      displayName: '',
       email: '',
       password: '',
       passwordRepeat: ''
     }
   }
+
+  handleSubmit = async event => {
+    event.preventDefault()
+    const { displayName, email, password, passwordRepeat } = this.state
+    if (password !== passwordRepeat) {
+      alert("password don't match")
+      return
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password)
+      await createUserProfileDocument(user, { displayName })
+    
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        passwordRepeat: ''
+      })
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target
+
+    this.setState({ [name]: value})
+  }
+
   render() {
+    const { displayName, email, password, passwordRepeat } = this.state
     return (
       <div className='sign-up'>
-        <h2>I don't have an account</h2>
+        <h2>I don't have a account</h2>
         <span>Sign up with your email and password</span>
-        <form>
+        <form className='sign-up-form' onSubmit={this.handleSubmit}>
+          <FormInput
+            name='displayName' 
+            type='text' 
+            value={displayName} 
+            required
+            onChange={this.handleChange}
+            label="Name"></FormInput>
           <FormInput
             name='email' 
             type='email' 
-            value={this.state.email} 
+            value={email} 
             required
+            onChange={this.handleChange}
             label="email"></FormInput>
           <FormInput
             name='password' 
             type='password' 
-            value={this.state.password} 
+            value={password} 
             required
+            onChange={this.handleChange}
             label="password"></FormInput>
           <FormInput
             name='passwordRepeat' 
             type='password' 
-            value={this.state.password} 
+            value={passwordRepeat} 
             required
-            label="password repeat"></FormInput>
-          <FormInput></FormInput>
+            onChange={this.handleChange}
+            label="confirm repeat"></FormInput>
           <CustomButton type="submit">Sign up</CustomButton>
         </form>
       </div>
--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import SignIn from '../../components/sign-in/sign-in'
 import SignUp from '../../components/sign-up/sign-up'
 
-// import "./sign-in-sign-up.scss";
+import "./sign-in-sign-up.scss";
 
 const SignInSignUpPage = () => (
   <div className="sign-in-sign-up">
     <SignIn></SignIn>
-    {/* <SignUp></SignUp> */}
+    <SignUp></SignUp>
   </div>
 )
 

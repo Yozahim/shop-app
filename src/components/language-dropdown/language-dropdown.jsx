c07b82641b25b4c8fb1542ef8c8767fd1dda38b8
Sobaniec <konrad.sobaniec@capgemini.com>
2020-07-31 13:49:26 +0200

    some styles

--- src/components/language-dropdown/language-dropdown.jsx
+++ src/components/language-dropdown/language-dropdown.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { ReactComponent as PlFlag } from '../../assets/poland.svg'
+import { ReactComponent as EnFlag } from '../../assets/uk.svg'
+
 import './language-dropdown.style.scss'
 
-const LanguageDropdown = () => (
-  <div className='language-dropdown'></div>
-)
+const LanguageDropdown = () => {
+  const handleClick = () => {
+    console.log('heheh')
+  }
+  
+  return (
+  <div className='language-dropdown'>
+    <PlFlag onClick={handleClick}/>
+    <EnFlag onClick={handleClick}/>
+  </div>
+)}
 
 export default LanguageDropdown

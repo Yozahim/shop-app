62949d16376269d3c3c504e7ea7796d9984ec74d
Sobaniec <konrad.sobaniec@capgemini.com>
2020-07-31 11:10:13 +0200

    contact page done

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './contact.style.scss'
 import CustomButton from '../../components/custom-button/custom-button'
@@ -6,7 +6,22 @@ import CustomButton from '../../components/custom-button/custom-button'
 // import contactImage from '../../assets/contact.jpg'
 const imageUrl = 'https://www.choicescreening.com/hubfs/assets/images/billboards/Billboard-conatct-us.jpg'
 
-const ContactPage = () => (
+const ContactPage = () => {
+  const [userDataAndMessage, setUserDataAndMessage] = useState({name: '', email: '', message: ''})
+
+  const { name, email, message } = userDataAndMessage
+
+  const handleSubmit = () => {
+    alert('Message: ' + message + ' is sent with help of this email adress: ' + email + ', ' + name)
+  }
+
+  const handleChange = event => {
+    const { value, name } = event.target
+
+    setUserDataAndMessage({...userDataAndMessage, [name]: value})
+  }
+  
+  return (
   <div className='contact-page'>
     <div className='top-image-container'>
       <div 
@@ -21,37 +36,69 @@ const ContactPage = () => (
     <div className='site-content'>
       <div className='middle-content'>
         <div className='left-site-content'>
-          <h2>Dear customer</h2>
+          <h2 className='header-text'>Dear customer</h2>
           <span>We feel honored that we can answer your questions and help with any unpleasantness, 
           so if you have a problem or question, simply write to us an e-mail on address provided or just call us!</span>
-          <h2>At Choice you always tak to a human!</h2>
+          <h2 className='header-text'>At Choice you always talk to a human!</h2>
           <span>Have questions about background screening? Our entire team receives specialized training regularly to ensure you're receiving the best information possible. From basic questions to complex compliance inquiries, we're here to help!</span>
           <p>Interested in learning more about our services? Our Account Executives take the time to discuss your existing background screening program and help you make smart decisions that best meet your needs.</p>
         </div>
         <div className='vertical-line'></div>
         <div className='right-site-content'>
-          <h2>Corporate Office</h2>
+          <h2 className='header-text'>Corporate Office</h2>
           <span>Adress: Jagodowa 6, 60-179 Poznań</span>
           <span>Phone: 555 978 546</span>
-          <span>E-mail: yoza-shop@gmail.com</span>
-          <h2>Direct Contact</h2>
+          <div>
+            <span>E-mail: </span>
+            <a href="mailto:yoza-shop@gmail.com">yoza-shop@gmail.com</a>
+          </div>
+          <h2 className='header-text'>Direct Contact</h2>
           <span>Phone: 731 986 995</span>
-          <span>E-mail: konrad.sobaniec@gmail.com</span>
-          <h2>Contact to the supplier</h2>
+          <div>
+            <span>E-mail: </span>
+            <a href="mailto:konrad.sobaniec@gmail.com">konrad.sobaniec@gmail.com</a>
+          </div>
+          <h2 className='header-text'>Contact to the supplier</h2>
           <span>Adress: Żelazna 7, 97-300 Piotrków Trybunalski</span>
           <span>Phone: 801 400 373</span>
-          <span>E-mail: dpd-contact@gmail.com</span>
+          <div>
+            <span>E-mail: </span>
+            <a href="mailto:dpd-contact@gmail.com">dpd-contact@gmail.com</a>
+          </div>
         </div>
       </div>
-      <div className='message-content'>
-        <h2 className='message-content-title'>Get in touch</h2>
-        <input placeholder='Name and Surname' type='text' className='message-input'></input>
-        <input placeholder='Email' type='text' className='message-input'></input>
-        <textarea placeholder='Message...' type='text' className='message-textarea'></textarea>
-        <CustomButton style={{ marginBottom: 20 }} type='submit'>Send</CustomButton>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='message-content'>
+          <h2 className='message-content-title'>Contact us today!</h2>
+          <input 
+            placeholder='Name'
+            name='name'
+            value={name} 
+            type='text'
+            onChange={handleChange}
+            required
+            className='message-input'></input>
+          <input 
+            placeholder='Email'
+            name='email' 
+            value={email} 
+            type='email'
+            onChange={handleChange}
+            required
+            className='message-input'></input>
+          <textarea 
+            placeholder='Message...'
+            name='message' 
+            value={message} 
+            type='text'
+            onChange={handleChange}
+            required
+            className='message-textarea'></textarea>
+          <CustomButton style={{ marginBottom: 20 }} type='submit'>Send</CustomButton>
+        </div>
+      </form>
     </div>
   </div>
-)
+)}
 
 export default ContactPage

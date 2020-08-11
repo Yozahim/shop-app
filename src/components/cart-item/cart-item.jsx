8e300eb0195545c49935420309fb4e45a790c237
Sobaniec <konrad.sobaniec@capgemini.com>
2020-08-11 13:14:31 +0200

    performance opt, pushing to heroku

--- src/components/cart-item/cart-item.jsx
+++ src/components/cart-item/cart-item.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 
 import './cart-item.style.scss'
 
@@ -12,4 +12,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity }}) => (
   </div>
 )
 
-export default CartItem
+export default memo(CartItem)
